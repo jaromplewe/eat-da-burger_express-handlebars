4256d96cdc7ae441b8edea0f8dc6e96a22875480
Jarom Plewe <jplewe23@gmail.com>
2020-12-29 11:39:52 -0700

    added controller file with routes to backend

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var burger = require('../models/burgers');
+var router = express.router();
+
+router.get('/', function (req, res) {
+    burger.selectAll(function (data) {
+        var handlebarsObject = {
+            burgers: data
+        };
+        res.render('index', handlebarsObject)
+    });
+});
+
+router.post('/burgers', function (req, res) {
+    burger.insertOne([
+        'burger_name'
+    ], [
+        req.body.burger_name
+    ], function() {
+        res.redirect('/');
+    })
+});
+
+router.put('/burgers/:id', function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({
+        devoured: true
+    }, condition, function (data) {
+        res.redirect('/');
+    })
+});
+
+module.exports = router;
+
